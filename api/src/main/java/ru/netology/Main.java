1b0a48f829c1e8dd54646f2faca0a21d8e428bb2
Leonid <ithedgehog@yandex.ru>
2023-01-17 00:51:02 +0300

    Secon commit

--- api/src/main/java/ru/netology/Main.java
+++ api/src/main/java/ru/netology/Main.java
@@ -1,7 +1,11 @@
 package ru.netology;
 
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        MyService myService = new MyService();
+        System.out.println(myService.getMyEntity());
+        System.out.println(myService.setMyEntity(new MyEntity("second")));
+        System.out.println(myService.getMyEntity());
     }
 }
--- db/src/main/java/ru/netology/Db.java
+++ db/src/main/java/ru/netology/Db.java
@@ -1,2 +1,27 @@
-package ru.netology;public class Db {
+package ru.netology;
+
+import java.util.UUID;
+
+public class Db {
+
+    private DbSetting dbSetting;
+    private MyEntity myEntity;
+
+    public Db(DbSetting dbSetting) {
+        this.dbSetting = dbSetting;
+        myEntity = new MyEntity("first");
+        myEntity.setId(UUID.randomUUID());
+    }
+
+    public DbSetting getDbSetting() {
+        return dbSetting;
+    }
+
+    public MyEntity getMyEntity() {
+        return myEntity;
+    }
+
+    public void setMyEntity(MyEntity myEntity) {
+        this.myEntity = myEntity;
+    }
 }
